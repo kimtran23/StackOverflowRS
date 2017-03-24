be36da662d25926de84629452ca2d030c50d63e2
catherinep003 <tuyetmai3000@gmail.com>
2017-03-24 19:54:11 -0400

    Used Pyspark to join the questions and questions_tags test CSV files: - Joined and ordered by user ID although it is ordered by String and not   integer - Count the number of occurrences of each tag for each user - Output result into a single file (CSV because text only seems to allow   one column)

--- src/user_tag_parser.py
+++ src/user_tag_parser.py
@@ -1,19 +1,29 @@
-import csv
-from ast import literal_eval
+import pyspark
 
-csv_filename = '../data/sorted_users.csv'
-user_tag_filename = '../data/user_tag.csv'
+conf = pyspark.SparkConf().setAppName('tagcount').setMaster('local[*]')
+sc = pyspark.SparkContext(conf=conf)
 
-with open(csv_filename, 'rb') as csvFile, open(user_tag_filename, 'wb') as outputFile:
-    datareader = csv.reader(csvFile)
-    datawriter = csv.writer(outputFile)
+q_filename = '../data/questions_test.csv'
+qt_filename = '../data/question_tags_test.csv'
 
-    for row in datareader:
-        if row[5] != 'NA':
-            currentUser = row[5]
-            list_tags = literal_eval(row[7])
+# Retrieving questions data
+rdd1 = sc.textFile(q_filename).map(lambda line: line.split(','))
+header1 = rdd1.first()
+rdd1 = rdd1.filter(lambda row: row != header1).toDF(header1)
+# Retrieving questions_tags data
+rdd2 = sc.textFile(qt_filename).map(lambda line: line.split(','))
+header2 = rdd2.first()
+rdd2 = rdd2.filter(lambda row: row != header2).toDF(header2)
 
-            for tag in list_tags:
-                print 'Writing tag for user ' + currentUser
-                r = [currentUser, tag]
-                datawriter.writerow(r)
+# Join the two CSVs
+keep = [rdd1.OwnerUserId, rdd2.Tag]
+join_result = rdd1.join(rdd2, rdd1.Id == rdd2.Id).select(*keep)
+
+# Order by user ID and count the number of tags per user
+id_tag_table = join_result.orderBy(rdd1.OwnerUserId)
+id_tag_table.show()
+id_tag_count = id_tag_table.groupBy(rdd1.OwnerUserId, rdd2.Tag).count()
+id_tag_count.show()
+
+# Save data into a single file
+id_tag_count.coalesce(1).write.format('com.databricks.spark.csv').save('../data/tagcount')
